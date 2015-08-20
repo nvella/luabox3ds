24cc4840d43183a3b9038cccbd37fbc7cc39835e
Nick Vella <nick@nxk.me>
2015-08-20 19:39:43 +1000

    remove unused defines

--- source/softkb.h
+++ source/softkb.h
@@ -2,10 +2,6 @@
 
 #include <3ds/gfx.h>
 
-#define SOFTKB_KEY_TAB "t"
-#define SOFTKB_KEY_CAPS "caps"
-#define SOFTKB_KEY_LSHIFT "shift"
-
 void SoftKb_Setup(gfxScreen_t, int, int);
 void SoftKb_Draw();
 int SoftKb_Handle(u32);
